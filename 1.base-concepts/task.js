28d5178dc21a318c733ac961d1eda89a9fd0602f
mrigor89 <sonin.igor1989@gmail.com>
2023-06-18 18:43:29 +0300

    Задачи 1 и 2 решены

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,20 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d =  Math.pow(b, 2) - 4*a*c
+  if(d == 0){
+    arr.push(-b/(2*a));
+  }else if(d > 0){
+    arr.push((-b + Math.sqrt(d))/(2*a));
+    arr.push((-b - Math.sqrt(d))/(2*a));
+  }
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  percent = percent/1200;
+  let credit = amount - contribution;
+  let mounthPay = credit*(percent + (percent / ((Math.pow((1 + percent), countMonths)) - 1)));
+  let total = mounthPay*countMonths;
+  return +total.toFixed(2);
 }
